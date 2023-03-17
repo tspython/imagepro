f16462117253ea5952cf449b1c46ea318aeb226f
tspython <tspython@hotmail.com>
2023-03-16 19:50:55 -0700

    add images concurrently

--- src/processing.rs
+++ src/processing.rs
@@ -50,6 +50,37 @@ where
     return resized_image;
 }
 
+//use image::{DynamicImage, GenericImageView};
+use rayon::prelude::*;
+//use std::path::Path;
+
+pub fn concat_images_vertically(image_paths: &[&str]) -> DynamicImage {
+    // Load all images and get their heights
+    let images: Vec<DynamicImage> = image_paths
+        .par_iter()
+        .map(|path| image::open(&Path::new(path)).unwrap())
+        .collect();
+    let heights: Vec<u32> = images.par_iter().map(|img| img.height()).collect();
+
+    // Compute the height of the final image
+    let total_height: u32 = heights.iter().sum();
+
+    // Create a new image with the same width as the first image and the combined height
+    let first_image = &images[0];
+    let width = first_image.width();
+    let mut result = DynamicImage::new_rgb8(width, total_height);
+
+    // Copy each image into the final image, stacked vertically
+    let mut y_offset = 0;
+    for (img, height) in images.iter().zip(heights.iter()) {
+        result.copy_from(img, 0, y_offset).unwrap();
+        y_offset += height;
+    }
+
+    // Return the final image
+    result
+}
+/*
 pub fn concat_images_vertically(image_paths: &[&str]) -> DynamicImage {
     // Load all images and get their heights
     let mut images: Vec<DynamicImage> = Vec::new();
@@ -79,4 +110,4 @@ pub fn concat_images_vertically(image_paths: &[&str]) -> DynamicImage {
     // Return the final image
     result
 }
-
+*/
